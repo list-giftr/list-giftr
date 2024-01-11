b79c110dad6f080385fabab6e1950d099b279b84
Chathan Driehuys <chathan@driehuys.com>
2024-01-11 18:06:11 +0000

    feat: basic idea list detail page

--- wishlists/core/urls.py
+++ wishlists/core/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from core import views
 
-urlpatterns = [path("ideas", views.IdeaListsView.as_view(), name="idea-lists")]
+urlpatterns = [
+    path("ideas/", views.IdeaListsView.as_view(), name="idea-lists"),
+    path(
+        "ideas/<uuid:pk>/", views.IdeaListDetailView.as_view(), name="idea-list-detail"
+    ),
+]
--- wishlists/core/views.py
+++ wishlists/core/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from core import models
@@ -11,7 +12,16 @@ class IdeaListsView(LoginRequiredMixin, ListView):
     model = models.IdeaList
     template_name = "core/idealist_list.html"
 
-    def get_queryset(self) -> QuerySet[Any]:
+    def get_queryset(self) -> QuerySet[models.IdeaList]:
         idea_lists = super().get_queryset()
 
         return idea_lists.filter(owner=self.request.user)
+
+
+class IdeaListDetailView(LoginRequiredMixin, DetailView):
+    context_object_name = "idea_list"
+    model = models.IdeaList
+    template_name = "core/idealist_detail.html"
+
+    def get_queryset(self) -> QuerySet[models.IdeaList]:
+        return super().get_queryset().filter(owner=self.request.user)
