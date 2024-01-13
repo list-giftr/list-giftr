d7181b7153df05c23c2cce6c6a4dfc2597e0c25d
Chathan Driehuys <chathan@driehuys.com>
2024-01-13 16:45:06 +0000

    chore: linting with ruff

--- wishlists/core/views.py
+++ wishlists/core/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
@@ -6,8 +7,8 @@ from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
 from core import models
