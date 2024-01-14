be807ed3057f2e481dbf270fdc233db06b1448f4
Chathan Driehuys <chathan@driehuys.com>
2024-01-14 02:34:11 +0000

    feat: favicon

--- wishlists/wishlists/settings.py
+++ wishlists/wishlists/settings.py
@@ -206,6 +206,7 @@ USE_TZ = True
 
 STATIC_ROOT = "/var/www/static"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
